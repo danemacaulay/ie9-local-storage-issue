123da84d1041f73ad707294ec01a9c11fccacd9a
Dane Macaulay <MacaulayD@DNB.com>
2014-09-26 10:12:06 -0500

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+if (localStorage.greeting) {
+	console.log('localStorage is set');
+	alert('Hello ' + localStorage.greeting);
+}
+else if (document.cookie !== 'localStorageIsSet'){
+	// Use a cookie to track the storage make sure we dont reset it if it should be set
+	localStorage.setItem('greeting', 'world');
+	document.cookie = 'localStorageIsSet';
+	console.log('localStorage is now set');
+}
+else {
+	console.log('localStorage not set');
+}
