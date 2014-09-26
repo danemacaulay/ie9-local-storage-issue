8088287c100abe93101e0c1d9ad2855f7f5f3676
Dane Macaulay <MacaulayD@DNB.com>
2014-09-26 10:45:28 -0500

    updates

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (localStorage.greeting) {
 else if (document.cookie !== 'localStorageIsSet'){
 	// Use a cookie to track the storage make sure we dont reset it if it should be set
 	localStorage.setItem('greeting', 'world');
-	document.cookie = 'localStorageIsSet=;expires=Thu, 01 Jan 2020 00:00:01 GMT;';
+	document.cookie = 'localStorageIsSet;expires=Thu, 01 Jan 2020 00:00:01 GMT;';
 	console.log('localStorage is now set');
 }
 else {
